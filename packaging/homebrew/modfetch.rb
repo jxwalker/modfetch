2b1fd5917e1915348a5e579568da64667c602753
James Walker <james@jameswalker.org.uk>
2026-05-05 12:20:16 +0100

    chore: update package metadata for v0.7.1

--- packaging/homebrew/modfetch.rb
+++ packaging/homebrew/modfetch.rb
@@ -1,25 +1,25 @@
 class Modfetch < Formula
   desc "Robust CLI/TUI downloader for LLM and Stable Diffusion assets"
   homepage "https://github.com/jxwalker/modfetch"
-  version "0.5.2"
+  version "0.7.1"
   on_macos do
     on_arm do
       url "https://github.com/jxwalker/modfetch/releases/download/v#{version}/modfetch_darwin_arm64"
-      sha256 "1cda41590d96a08f255c6e230e0cc23e73491b488ea0df500f73258e593ddff6"
+      sha256 "9b1eb0498197e10616c13996839aba4648800f4ac9d6ed8f17bdc9c4a54ca464"
     end
     on_intel do
       url "https://github.com/jxwalker/modfetch/releases/download/v#{version}/modfetch_darwin_amd64"
-      sha256 "a53533b5202b450c32e76d1c872c824ca4d0b42dada0e9049799c81bb08eb4bd"
+      sha256 "174f34693fcae4476633d3c670a8f3366f3002baa15ea1ad6882d3f25ede1e6b"
     end
   end
   on_linux do
     on_arm do
       url "https://github.com/jxwalker/modfetch/releases/download/v#{version}/modfetch_linux_arm64"
-      sha256 "f504fc75c2d2bdf011f817ec5a8d2742649576ccc192f4e87476e4f17d3bed41"
+      sha256 "893e51802932381bc14d472d94002813c491c5170f14e50573e0b30cf873c868"
     end
     on_intel do
       url "https://github.com/jxwalker/modfetch/releases/download/v#{version}/modfetch_linux_amd64"
-      sha256 "a9394e97bffec284b3c65bd16a02e62191be5aa11b9997b9a5cbd2cd6dc10b84"
+      sha256 "c7db15650986c32cf4b1550699e624a929e21b926194a2523896fbacec95276c"
     end
   end
 
@@ -39,4 +39,3 @@ class Modfetch < Formula
     system "#{bin}/modfetch", "version"
   end
 end
-
